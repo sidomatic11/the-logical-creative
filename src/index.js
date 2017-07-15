87c8f4ffdfdcb916281014bce70a2e44f52319e6
siddhant <sidomatic11@gmail.com>
2017-07-15 17:26:20 +0530

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Routes from './Routes';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
-registerServiceWorker();
